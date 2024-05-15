43e086bc49196d12557cb6cb8445f44ee9bf7127
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-14 23:29:26 -0400

    Update MergeStringAlternatively.java

--- JavaAlgorithms/MergeStringAlternatively.java
+++ JavaAlgorithms/MergeStringAlternatively.java
@@ -1,1 +1,19 @@
-
+// ***************************************************************************************************************
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder fr = new StringBuilder();
+      // Set up a string holder for tracking
+        int i = 0;
+        while(i < word1.length() || i < word2.length()) {
+            if(i < word1.length()){
+                fr.append(word1.charAt(i));
+            }
+            if(i < word2.length()){
+                fr.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return fr.toString();
+    }
+}
+// ****************************************************************************************************************
