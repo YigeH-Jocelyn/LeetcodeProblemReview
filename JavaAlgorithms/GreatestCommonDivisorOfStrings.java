37e364187e80dfa3bb7ee5d36f81620d5300317e
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-15 22:00:19 -0400

    Update GreatestCommonDivisorOfStrings.java

--- JavaAlgorithms/GreatestCommonDivisorOfStrings.java
+++ JavaAlgorithms/GreatestCommonDivisorOfStrings.java
@@ -1,1 +1,16 @@
+// *****************************************************************************************************
+  class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if ((str1 + str2).equals(str2 + str1)) {
+            return str1.substring(0, gcd(str1.length(), str2.length()));
+        } else {
+            return "";
+        }
+    }
 
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+      // Reduce the problem to smaller instances until the base case is reached.
+    }
+}
+// ******************************************************************************************************
