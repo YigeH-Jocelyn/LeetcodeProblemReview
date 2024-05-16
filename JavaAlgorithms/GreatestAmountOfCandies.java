cc8e2578c46fdd1d28b9897523fa5ce1db29a3b2
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-15 22:20:47 -0400

    Update GreatestAmountOfCandies.java

--- JavaAlgorithms/GreatestAmountOfCandies.java
+++ JavaAlgorithms/GreatestAmountOfCandies.java
@@ -1,1 +1,21 @@
+// **************************************************************************************************
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandy = 0;
+        for (int each : candies){
+          // For each element "each" in the array "candies"
+            maxCandy = Math.max(maxCandy, each);
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int each : candies){
+            if(each + extraCandies >= maxCandy){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
 
+    }
+}
+// ****************************************************************************************************
