a935ee72d1229472b163da526ba8a6d3c7304cb5
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-17 00:03:19 -0400

    Update CanPlaceFlowers.java

--- JavaAlgorithms/CanPlaceFlowers.java
+++ JavaAlgorithms/CanPlaceFlowers.java
@@ -8,13 +8,14 @@ class Solution {
                 return false;
             return true;
         }
-            
+            // for array with size of 1.
         for (int i = 0; i < flowerbed.length; i++) {
             if(i==0){
                 if(flowerbed[i+1] == 0 && flowerbed[i] == 0){
                     flowerbed[i] = 1;
                     n--;
                 }
+                // to fill the first cell with 1 if it and the cell after it are 0
                 continue;
             }
             if(i==flowerbed.length-1){
@@ -22,11 +23,13 @@ class Solution {
                     flowerbed[flowerbed.length-1] = 1;
                     n--;
                 }
+                // to fill the last cell with 1 if it and the cell before it are 0
                  continue;
             }
             if(flowerbed[i-1] == 0 && flowerbed[i+1] == 0 && flowerbed[i] == 0){
                 flowerbed[i] = 1;
                 n--;
+                // fill the cells with 1 if the cells before and after it are also 0.
                 System.out.println(n);
             }
         }
