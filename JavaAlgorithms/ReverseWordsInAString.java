443891f626172b3a499e547e492dbd6f8893368e
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-18 15:07:00 -0400

    Update ReverseWordsInAString.java

--- JavaAlgorithms/ReverseWordsInAString.java
+++ JavaAlgorithms/ReverseWordsInAString.java
@@ -1,1 +1,24 @@
+// *****************************************************************************************
+class Solution {
+    public String reverseWords(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
 
+        String[] words = s.split(" ");
+        StringBuilder fr = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (!words[i].isEmpty()) {
+                fr.append(words[i]);
+                if (i != 0) {
+                    fr.append(" ");
+                }
+            }
+        }
+
+        return fr.toString().trim();
+    }
+}
+
+// *******************************************************************************************
