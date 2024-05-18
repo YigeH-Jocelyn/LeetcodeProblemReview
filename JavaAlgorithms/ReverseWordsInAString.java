25058ef2f1b07e08116ff07e8dbe62895d565ac8
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-18 15:13:51 -0400

    Update ReverseWordsInAString.java

--- JavaAlgorithms/ReverseWordsInAString.java
+++ JavaAlgorithms/ReverseWordsInAString.java
@@ -4,8 +4,10 @@ class Solution {
         if (s == null || s.length() == 0) {
             return s;
         }
+        // Chect if the string "s" is null or the length of s is 0
 
         String[] words = s.split(" ");
+        // Split the word/define word using a space.
         StringBuilder fr = new StringBuilder();
 
         for (int i = words.length - 1; i >= 0; i--) {
@@ -13,11 +15,13 @@ class Solution {
                 fr.append(words[i]);
                 if (i != 0) {
                     fr.append(" ");
+            // if the word is define where there is no space between the characters, the words are appended together and separated by a space. != 0 ensures that there is no space at the end of the reverse string.
                 }
             }
         }
 
         return fr.toString().trim();
+        // Trim avoids the unintended leading and trailing whitespace
     }
 }
 
