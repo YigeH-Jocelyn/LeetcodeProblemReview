80c83ef7cbd9925fd7a93db1f9f90247dc991c83
YigeH-Jocelyn <164921359+YigeH-Jocelyn@users.noreply.github.com>
2024-05-22 12:05:35 -0400

    Update ProductOfArrayExceptSelf.java

--- JavaAlgorithms/ProductOfArrayExceptSelf.java
+++ JavaAlgorithms/ProductOfArrayExceptSelf.java
@@ -1,1 +1,20 @@
-
+// ********************************************************************************************
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int right = 1;
+        int[] result = new int[nums.length];
+        result[0] = 1;
+      // Initialize result with 1, so the first element is not going through the following calculation.
+        for(int i = 1; i < nums.length; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
+      // Calculate the product of the left side, except the nums[i] it self
+        for(int i = nums.length -1; i >= 0; i--){
+            result[i] *= right;
+            right *= nums[i];
+        }
+      // Calculate the product on the right side, except nums[i] itself.
+        return result;
+    }
+}
+// *********************************************************************************************
